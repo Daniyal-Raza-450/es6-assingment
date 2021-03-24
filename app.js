bfdda47b442b72b6abdf839ef991bad3e98c895a
unknown <daniyalraza056@gmail.com>
2021-03-24 11:37:16 +0500

    assingment complete

---
+++ app.js
@@ -0,0 +1,32 @@
+// destracturing of nested array
+
+let  c = [' mango','apple','orange','banana'] [' school','computer','keys']
+let a,b;
+[a, ...b, ...rest] = c;
+console.log(a, b)
+
+// let a = [2,3,4,5,6,7,8]
+// let b,c;
+// [c, ...b] =  a 
+
+// =============Object keys
+
+// let obj ={
+//     a:'name',
+//     b:'ali',
+//     c:'ahmad',
+
+
+// }
+// console.log(Object.keys(obj))
+
+// ========Object value
+
+// let obj ={
+//     a:'name',
+//     b:'ali',
+//     c:'ahmad',
+
+
+// }
+// console.log(Object.values(obj))
